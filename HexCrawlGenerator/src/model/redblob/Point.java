f9a0b1a4351d5cbcbc14cedda80ae4881f4686e8
BuildTools <unconfigured@null.spigotmc.org>
2018-07-20 17:38:36 +0800

    RedBlob Implementation, is way better.

---
+++ HexCrawlGenerator/src/model/redblob/Point.java
@@ -0,0 +1,11 @@
+package model.redblob;
+import java.util.*;
+public class Point {
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public final double x;
+    public final double y;
+}
