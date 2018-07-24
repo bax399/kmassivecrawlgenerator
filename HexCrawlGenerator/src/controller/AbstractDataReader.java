8aa1563fbd48cf62ad30613d8000c3ab050a25a4
BuildTools <unconfigured@null.spigotmc.org>
2018-07-24 11:56:59 +0800

    Sets up FileProcessing with Properties

---
+++ HexCrawlGenerator/src/controller/AbstractDataReader.java
@@ -0,0 +1,69 @@
+package controller;
+import java.util.*;
+import java.io.*;
+
+import model.*;
+import view.*;
+
+public abstract class AbstractDataReader<E> 
+{
+	protected abstract E processline(String[] line, E storage, int linenum);
+	//Format:
+	//<ID>,<Strat1>,<Strat2>,<...9>
+	public E readFile(String filename, E inStorage)
+	{
+		FileReader fr=null;
+		BufferedReader br=null;
+		String line;
+		String[] csvline;
+		int linenum=-1; 
+		
+		E storage = inStorage;
+		
+		try
+		{
+			fr = new FileReader(filename);
+			br = new BufferedReader(fr);
+			
+			//Assuming not reading header lines.
+			line = br.readLine();
+			linenum++;
+			while(line!=null)
+			{				
+				//Process 				
+				csvline = line.split(",");
+
+				storage = processline(csvline, storage, linenum);
+				
+				line = br.readLine();	
+				linenum++;
+			}
+
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			System.out.println("Plant Read Error.");
+		}
+		finally
+		{
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}			
+		}
+		
+		return storage;
+	}
+	
+
+}
---
+++ HexCrawlGenerator/src/controller/FileProcessor.java
@@ -0,0 +1,48 @@
+package controller;
+import controller.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class FileProcessor {
+	
+	public void processFile(String filename)
+	{
+		FileReader fr = null;
+		BufferedReader br = null;
+		String line = null;
+		TypeReader tr = null;	
+		//read until a line exists that isn't a comment or empty.
+		
+		try
+		{
+			fr = new FileReader(filename);
+			br = new BufferedReader(fr);
+			line = br.readLine();
+			tr = new TypeReader();
+			while(line != null)
+			{
+				while( (line.trim().isEmpty()) || (line.startsWith("#")) || (!line.startsWith("TYPE")) )
+				{
+					line = br.readLine();
+				}
+				
+				tr.processString(br);
+				
+					//Deprecated, not doing this method.
+						//String classname = split[1].trim();	
+						//classname = classname.substring(0,1).toUpperCase() + classname.substring(1).toLowerCase();
+						//Class<TypeReader> type = (Class<TypeReader>) Class.forName(classname+"Reader");
+
+			}
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+			
+		
+		
+		
+	}
+}
